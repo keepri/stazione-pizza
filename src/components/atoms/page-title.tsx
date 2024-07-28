c8d62a999fe3b6a7f27fbb80b6fead38db8351ba
KIPRI <hi@kipri.dev>
2024-07-28 13:51:38 +0300

    style: use css instead of js for page title side selection

--- src/components/atoms/page-title.tsx
+++ src/components/atoms/page-title.tsx
@@ -1,10 +1,9 @@
+import { clsx } from "clsx";
 import { ParentProps, children } from "solid-js";
 
 type Props = ParentProps & {
     side?: "left" | "right";
 };
-
-const BAR_CLASSES = "w-full h-16 bg-stz-primary";
 
 export function PageTitle(props: Props) {
     props.side ??= "left";
@@ -16,11 +15,22 @@ export function PageTitle(props: Props) {
 
     return (
         <div class="flex justify-between items-center py-14">
-            {isRight && <span class={BAR_CLASSES} />}
-            <h1 class="text-8xl font-extrabold text-stz-dark px-36">
+            <h1
+                class={clsx(
+                    "text-8xl font-extrabold text-stz-dark px-36",
+                    isRight && "order-2",
+                    isLeft && "order-1",
+                )}
+            >
                 {resolved()}
             </h1>
-            {isLeft && <span class={BAR_CLASSES} />}
+            <span
+                class={clsx(
+                    "w-full h-16 bg-stz-primary",
+                    isRight && "order-1",
+                    isLeft && "order-2",
+                )}
+            />
         </div>
     );
 }
