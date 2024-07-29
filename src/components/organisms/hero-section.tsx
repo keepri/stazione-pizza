9b26c30f87a5665e4a66cd648791c82d59f5943d
KIPRI <hi@kipri.dev>
2024-07-29 21:16:36 +0300

    feat: responsive hero section

--- src/components/organisms/hero-section.tsx
+++ src/components/organisms/hero-section.tsx
@@ -5,12 +5,12 @@ import { Section } from "../atoms/section";
 
 export function HeroSection() {
     return (
-        <Section class="flex flex-wrap justify-between">
+        <Section class="flex max-xl:flex-wrap justify-between max-xl:gap-14 max-xl:max-w-xl">
             <div class="max-w-prose">
                 <h1 class="mb-8 leading-tight text-8xl text-stz-dark font-poppins font-extrabold">
                     Pizza <span class="text-stz-primary">Napoletană</span>
                 </h1>
-                <P class="mb-12 text-xl leading-relaxed">
+                <P class="mb-16 text-xl leading-relaxed">
                     Descoperă adevărata pizza napoletană chiar în inima
                     Clujului! Fiecare pizza este pregătită cu ingrediente
                     autentice și multă pasiune, pentru a-ți oferi o experiență
@@ -19,8 +19,9 @@ export function HeroSection() {
                 <ButtonLink href={Path.Menu}>DESCOPERĂ MENIUL</ButtonLink>
             </div>
             <img
-                width={500}
-                height={500}
+                width={550}
+                height={550}
+                class="max-lg:mx-auto"
                 src="src/assets/images/hero-pizza.png"
                 alt="pizza"
             />
